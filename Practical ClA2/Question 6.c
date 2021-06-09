69a6a1b0649774832a52f517019e5f502cd21878
akashkumar0987d <80807783+akashkumar0987d@users.noreply.github.com>
2021-06-09 20:44:07 +0530

    Create Question 6.c

---
+++ Practical ClA2/Question 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,temp;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements of array: ");
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<0)
+        count++;
+    }
+    printf("No of negative number in array is %d\n",count);
+    return 0;
+}
